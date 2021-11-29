cfb4e7207516f9d2c16591e38afa54b61863fcb7
ShenYj <shenyanjie123@foxmail.com>
2021-11-29 14:01:25 +0800

    demo

--- Examples/iOSDemo/ViewController.swift
+++ Examples/iOSDemo/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import PPILibrary
 
 class ViewController: UIViewController {
+    
+    private lazy var btn: UIButton = {
+        let button = UIButton()
+        button.setTitle("test", for: .normal)
+        button.backgroundColor = UIColor.blue
+        button.layer.cornerRadius = 5
+        button.layer.maskedCorners = .allCorner
+        button.titleLabel?.font = UIFont.SYS_F15
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.addSubview(btn)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        btn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        btn.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
     }
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PPILibrary
+
 Personal plug-in library
+
+## 功能介绍
+
+- [x] `Core` 核心基础扩展， 不依赖于任何三方库
